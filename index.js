2febf455d816c5e77f1574e8a5b135c9ed326183
yeeric01 <yeeric01@gmail.com>
2021-01-05 11:16:00 -0600

    added improvements

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+http
+  .createServer(function (req, res) {
+    const pathname = url.parse(req.url, true).pathname;
+    if (pathname == "/favicon.ico") return res.end();
+    let filename;
+    if (pathname == "/") filename = "./index.html";
+    else filename = "." + pathname + ".html";
+    console.log(filename);
+    if (
+      filename == "./index.html" ||
+      filename == "./contact-me.html" ||
+      "./about.html"
+    ) {
+      fs.readFile(filename, function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      console.log("false");
+      fs.readFile("./404.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    }
+  })
+  .listen(8080, () => {
+    console.log("Server made");
+  });
