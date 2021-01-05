8771af658fd178ceabae2a4d7d01108237f8b28a
yeeric01 <yeeric01@gmail.com>
2021-01-05 11:31:32 -0600

    got rid of console.log's and shortened code

--- index.js
+++ index.js
@@ -9,26 +9,18 @@ http
     let filename;
     if (pathname == "/") filename = "./index.html";
     else filename = "." + pathname + ".html";
-    console.log(filename);
-    if (
-      filename == "./index.html" ||
-      filename == "./contact-me.html" ||
-      filename == "./about.html"
-    ) {
-      console.log("true");
-      fs.readFile(filename, function (err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        fs.readFile("./404.html", function (err2, data2) {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data2);
+        });
         return res.end();
-      });
-    } else {
-      console.log("false");
-      fs.readFile("./404.html", function (err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        return res.end();
-      });
-    }
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
   })
   .listen(8080, () => {
     console.log("Server made");
